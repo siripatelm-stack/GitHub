e9edf9add79794cb706e7d3b802547aa25a91c04
siripatelm-stack <siripatelm@gmail.com>
2026-01-19 20:11:07 +0530

    wrote in master branch

--- file2.py
+++ file2.py
@@ -1,9 +1,15 @@
+#write something in MASTER BRANCH 
+#butterfly half wing
 #to resolve conflict in file2.py
 #made changes for that
  
-<<<<<<< HEAD
+"""<<<<<<< HEAD
 
 =======
 for i in range(5):
   print("hello")
->>>>>>> bb0af91 (Revert "deleted file1.py and file2.py")
+>>>>>>> bb0af91 (Revert "deleted file1.py and file2.py")"""
+for i in range (5):
+  print('*'*i)
+for i in range (5,0,-1):
+  print('*'*i)
